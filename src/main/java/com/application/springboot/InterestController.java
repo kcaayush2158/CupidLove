6107c9864a3a8e61958462deab23f3233507b3b6
System Administrator <aayushkc2158@gmail.com>
2021-07-10 15:56:10 +0545

    interest  api added

--- src/main/java/com/application/springboot/InterestController.java
+++ src/main/java/com/application/springboot/InterestController.java
@@ -6,10 +6,7 @@ import com.application.springboot.service.InterestService;
 import com.application.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,7 +25,7 @@ public class InterestController {
         return ResponseEntity.ok().body(this.interestService.findInterests(user));
     }
 
-    @GetMapping("/interest/save")
+    @PostMapping("/interest/save")
     public ResponseEntity<Interests> saveInterest(@RequestParam("email")String email,@RequestParam("interest")String interest){
         User user = userService.findExistingEmail(email);
         Interests interests = new Interests();
