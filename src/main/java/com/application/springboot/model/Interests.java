977609d9c537bf72c4d0738d26e865e24e08fb8c
System Administrator <aayushkc2158@gmail.com>
2021-07-10 18:54:45 +0545

    interest  api added

--- src/main/java/com/application/springboot/model/Interests.java
+++ src/main/java/com/application/springboot/model/Interests.java
@@ -5,9 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -15,6 +13,7 @@ import javax.persistence.ManyToOne;
 @NoArgsConstructor
 public class Interests {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @ManyToOne
     private User user;
